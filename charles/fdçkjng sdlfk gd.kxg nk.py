96d94a8bd834ec70968a359d9c9ec0886851f365
Charles Dos Santos Duarte <charlesdsantosd@gmail.com>
2025-09-02 11:41:29 -0300

    oi

---
+++ charles/fdçkjng sdlfk gd.kxg nk.py
@@ -0,0 +1,6 @@
+x = int(input("digite um numero: "))
+y = int(input("digite um numero: "))
+if x >= y:
+    print(y,x)
+else:
+    print(x,y)
---
+++ charles/peido.py
@@ -0,0 +1,7 @@
+x = float(input("digite um numero: "))
+if x > 0:
+     print("o numero é positivo.")
+elif x < 0:
+     print("o numero é negativo")
+else:
+     print("o numero é zero")
